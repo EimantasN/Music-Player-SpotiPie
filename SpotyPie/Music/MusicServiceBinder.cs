b9c53947f16f97467eb4d04a85c783a25d16725a
Eimantas Noreika <noreika.eimantas@gmail.com>
2019-06-13 22:10:03 +0300

    Added binder class to connect fragment to service

---
+++ SpotyPie/Music/MusicServiceBinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.OS;
+using SpotyPie.Player;
+
+namespace SpotyPie.Music
+{
+    public class MusicServiceBinder : Binder
+    {
+        private MusicService Service { get; set; }
+
+        public MusicServiceBinder(MusicService service)
+        {
+            Service = service;
+        }
+
+        internal void SetMusicServiceUpdateCallback(Player.Player player)
+        {
+            Service?.SetCallback(player);
+        }
+    }
+}
