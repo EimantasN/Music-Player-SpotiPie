7ddf6567345f75883f0b870b20820ef7a05e0a38
Eimantas <eimantas@kiro.tech>
2019-06-23 07:55:48 +0300

    Added new realm model to store playing music list.

---
+++ Mobile_Api/Models/Realm/Music.cs
@@ -0,0 +1,31 @@
+﻿using Realms;
+
+namespace Mobile_Api.Models.Realm
+{
+    public class Music : RealmObject
+    {
+        public int Id { get; set; }
+
+        public bool IsPlaying { get; set; }
+
+        public Realm_Songs Song { get; set; }
+
+        public Music() { }
+
+        public Music(Realm_Songs song, bool playing = false)
+        {
+            Id = song.Id;
+            IsPlaying = playing;
+            song.IsPlaying = playing;
+            Song = song;
+        }
+
+        public Music(Songs song, bool playing = false)
+        {
+            Id = song.Id;
+            IsPlaying = playing;
+            song.IsPlaying = playing;
+            Song = new Realm_Songs(song);
+        }
+    }
+}
